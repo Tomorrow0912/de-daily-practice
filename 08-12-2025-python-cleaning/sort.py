96c6ace09da6b77ad29b8059923784f8476d7d9a
Thomas Morrow <thomasmorrow574@gmail.com>
2025-12-09 19:33:08 +1100

    09-12-2025 Python Cleaning V2

--- 08-12-2025-python-cleaning/sort.py
+++ 08-12-2025-python-cleaning/sort.py
@@ -3,4 +3,15 @@ data = pd.read_csv('./lotr_characters.csv')
 
 df=pd.DataFrame(data)
 df["race"] = df["race"].str.lower()
-print(df[['race']])
+df["race"] = df["race"].str.strip()
+df["race"] = df["race"].fillna("unknown")
+#print(data)
+print("this is the raw csv")
+print(data)
+
+df.to_csv('cleaned_lotr_characters.csv', index=False)
+
+cleaned_data=pd.read_csv('./cleaned_lotr_characters.csv')
+
+print("this is the cleaned data")
+print(cleaned_data)
